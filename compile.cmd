4caa67032f8eaaa43f0ca975720f40a16537a363
Techgirl Sophie <129156323+0daysophie@users.noreply.github.com>
2025-10-20 18:59:11 +0200

    Update pyinstaller command to target i386 architecture and to disable upx compression

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
 curl https://raw.githubusercontent.com/duino-coin/duino-coin/refs/heads/master/PC_Miner.py -o PC_Miner.py
 python3 -m pip install multiprocessing pypresence py-cpuinfo psutil requests pyinstaller colorama
-pyinstaller --noconfirm --onefile --console --icon 1.ico --clean PC_Miner.py
+pyinstaller --noconfirm --onefile --console --icon 1.ico --clean --noupx --target-arch i386 PC_Miner.py
 mv .\dist\PC_Miner.exe .\PC_Miner.exe
